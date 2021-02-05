e8e79d2841dcd4018097d0ec1d462c3d2102a9d4
Malhar Patel <leofierus@gmail.com>
2021-02-05 14:18:57 +0530

    Update Find the maximum and minimum element in an array.cpp

--- Array/Find the maximum and minimum element in an array.cpp
+++ Array/Find the maximum and minimum element in an array.cpp
@@ -1,7 +1,6 @@
 class Solution{
   public:
     int middle(int A, int B, int C){
-        //code here//Position this line where user code will be pasted.
         vector<int> v;
         v.push_back(A);v.push_back(B);v.push_back(C);
         sort(v.begin(),v.end());
