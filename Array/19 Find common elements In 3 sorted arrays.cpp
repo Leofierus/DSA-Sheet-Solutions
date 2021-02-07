ee602890b31d6771e6222a9444d2d9b9586f3fe5
Malhar Patel <leofierus@gmail.com>
2021-02-08 00:45:50 +0530

    Update 19 Find common elements In 3 sorted arrays.cpp

--- Array/19 Find common elements In 3 sorted arrays.cpp
+++ Array/19 Find common elements In 3 sorted arrays.cpp
@@ -19,15 +19,11 @@ class Solution
             if(n1==0 || n2==0 || n3==0) return temp;
             int count=0;
             for(int i=0;i<n1;i++) if(!bS(B,0,n2-1,A[i])) lol[i]=-1,count++;
-            for(int i=0;i<n1;i++){
-                if(lol[i]!=-1){
-                    if(!bS(C,0,n3-1,A[i])) lol[i]=-1,count++;
-                }
-            }
+            for(int i=0;i<n1;i++) if(lol[i]!=-1) if(!bS(C,0,n3-1,A[i])) lol[i]=-1,count++;
             if(count==n1) return temp;
             else{
                 vector<int> v;
-                for(int i=0;i<n1;i++) if(lol[i]!=-1 && find(v.begin(),v.end(),A[i])==v.end()) v.push_back(A[i]);
+                for(int i=0;i<n1;i++) if(lol[i]!=-1&&find(v.begin(),v.end(),A[i])==v.end()) v.push_back(A[i]);
                 return v;
             }
         }
